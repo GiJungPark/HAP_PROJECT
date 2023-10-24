9f136880bb311a7cf2a4a9706f8358af2aad3932
GiJung <weeds2577@gmail.com>
2023-10-25 03:24:30 +0900

    feat: create signUp controller

--- src/main/java/me/gijung/HAP/controller/UserController.java
+++ src/main/java/me/gijung/HAP/controller/UserController.java
@@ -1,2 +1,29 @@
-package me.gijung.HAP.controller;public class UserController {
+package me.gijung.HAP.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.gijung.HAP.dto.UserDto;
+import me.gijung.HAP.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("")
+    public ResponseEntity<UserDto.ResponseMessage> singUp(@RequestBody UserDto.RequestSingUp request) {
+        String message = userService.singUp(request);
+        UserDto.ResponseMessage response = new UserDto.ResponseMessage(HttpStatus.OK, message);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+
 }
