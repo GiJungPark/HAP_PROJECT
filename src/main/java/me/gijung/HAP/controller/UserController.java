5c9b52bafc61a0a65f4f70d2176e3f242a0a55ad
GiJung <weeds2577@gmail.com>
2023-10-25 18:46:09 +0900

    feat: create controller check duplicate email, nickname, phone

--- src/main/java/me/gijung/HAP/controller/UserController.java
+++ src/main/java/me/gijung/HAP/controller/UserController.java
@@ -5,9 +5,11 @@ import me.gijung.HAP.dto.UserDto;
 import me.gijung.HAP.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,10 +27,42 @@ public class UserController {
         return ResponseEntity.ok().body(response);
     }
 
+    @GetMapping("/email")
+    public ResponseEntity<UserDto.ResponseMessage> checkDuplicateEmail(@RequestParam("email") String email) {
+        String message = userService.checkDuplicateEmail(email);
+        UserDto.ResponseMessage response = new UserDto.ResponseMessage(HttpStatus.OK, message);
+
+        return ResponseEntity.ok().body(response);
+    }
+    @GetMapping("/nickname")
+    public ResponseEntity<UserDto.ResponseMessage> checkDuplicateNickname(@RequestParam("nickname") String nickname) {
+        String message = userService.checkDuplicateNickname(nickname);
+        UserDto.ResponseMessage response = new UserDto.ResponseMessage(HttpStatus.OK, message);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/phone")
+    public ResponseEntity<UserDto.ResponseMessage> checkDuplicatePhone(@RequestParam("phone") String phone) {
+        String message = userService.checkDuplicatePhone(phone);
+        UserDto.ResponseMessage response = new UserDto.ResponseMessage(HttpStatus.OK, message);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+
     @PostMapping("/login")
     public ResponseEntity<UserDto.ResponseToken> login(@RequestBody UserDto.RequestLogin request) {
         String token = userService.login(request);
         UserDto.ResponseToken response = new UserDto.ResponseToken(HttpStatus.OK, token);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<UserDto.ResponseMessage> logout() {
+        String message = userService.logout();
+        UserDto.ResponseMessage response = new UserDto.ResponseMessage(HttpStatus.OK, message);
 
         return ResponseEntity.ok().body(response);
     }
