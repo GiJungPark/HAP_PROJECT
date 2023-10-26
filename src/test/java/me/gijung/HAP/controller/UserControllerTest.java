75979970a9223c7084be7d0ea5667a928a73d0d3
GiJung <weeds2577@gmail.com>
2023-10-26 16:15:18 +0900

    test: create test for find email, change password function

--- src/test/java/me/gijung/HAP/controller/UserControllerTest.java
+++ src/test/java/me/gijung/HAP/controller/UserControllerTest.java
@@ -17,12 +17,15 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 
@@ -248,4 +251,147 @@ public class UserControllerTest {
 
         assertThat(message).isEqualTo(ErrorCode.INVALID_PASSWORD.getMessage());
     }
+
+
+    @Test
+    @DisplayName("아이디 찾기 성공")
+    @WithMockUser("USER")
+    void find_email() throws Exception {
+
+        // given
+        String url = "/api/v1/user/email";
+        String email = "email";
+
+        MultiValueMap<String, String> info = new LinkedMultiValueMap<>();
+        info.add("name", "박기중");
+        info.add("phone", "010-0000-0000");
+
+        when(userService.findEmail(any(), any()))
+                .thenReturn(email);
+
+        // when
+        MvcResult result = mockMvc.perform(get(url).with(csrf())
+                        .params(info))
+                .andDo(print())
+                .andReturn();
+
+        // then
+        String response = result.getResponse().getContentAsString();
+        String message = JsonPath.parse(response).read("$.email");
+
+        assertThat(message).isEqualTo(email);
+    }
+
+    @Test
+    @DisplayName("아이디 찾기 실패 - 해당 전화번호로 가입된 정보가 없음")
+    @WithMockUser("USER")
+    void find_email_fail_not_found_phone() throws Exception {
+
+        // given
+        String url = "/api/v1/user/email";
+
+        MultiValueMap<String, String> info = new LinkedMultiValueMap<>();
+        info.add("name", "박기중");
+        info.add("phone", "010-0000-0000");
+
+        when(userService.findEmail(any(), any()))
+                .thenThrow(new AppException(ErrorCode.PHONE_NOTFOUND));
+
+        // when
+        MvcResult result = mockMvc.perform(get(url).with(csrf())
+                        .params(info))
+                .andDo(print())
+                .andReturn();
+
+        // then
+        String response = result.getResponse().getContentAsString();
+        String message = JsonPath.parse(response).read("$.message");
+
+        assertThat(message).isEqualTo(ErrorCode.PHONE_NOTFOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("아이디 찾기 실패 - 올바르지 않은 이름")
+    @WithMockUser("USER")
+    void find_email_fail_invalid_name() throws Exception {
+
+        // given
+        String url = "/api/v1/user/email";
+
+        MultiValueMap<String, String> info = new LinkedMultiValueMap<>();
+        info.add("name", "박기중");
+        info.add("phone", "010-0000-0000");
+
+        when(userService.findEmail(any(), any()))
+                .thenThrow(new AppException(ErrorCode.INVALID_NAME));
+
+        // when
+        MvcResult result = mockMvc.perform(get(url).with(csrf())
+                        .params(info))
+                .andDo(print())
+                .andReturn();
+
+        // then
+        String response = result.getResponse().getContentAsString();
+        String message = JsonPath.parse(response).read("$.message");
+
+        assertThat(message).isEqualTo(ErrorCode.INVALID_NAME.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("비밀번호 변경 성공")
+    @WithMockUser("USER")
+    void change_password() throws Exception {
+
+        // given
+        String url = "/api/v1/user/password";
+        String email = "email";
+        String password = "password";
+        String success_message = "비밀번호가 변경되었습니다.";
+
+        when(userService.changePassword(any(), any()))
+                .thenReturn(success_message);
+
+        // when
+        MvcResult result = mockMvc.perform(post(url).with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserDto.RequestChangePassword(email, password))))
+                .andDo(print())
+                .andReturn();
+
+        // then
+        String response = result.getResponse().getContentAsString();
+        String message = JsonPath.parse(response).read("$.message");
+
+        assertThat(message).isEqualTo(success_message);
+    }
+
+    @Test
+    @DisplayName("비밀번호 변경 실패 - 해당 이메일로 가입된 정보가 없음")
+    @WithMockUser("USER")
+    void change_password_fail_not_found_phone() throws Exception {
+
+        // given
+        String url = "/api/v1/user/password";
+        String email = "email";
+        String password = "password";
+
+        when(userService.changePassword(any(), any()))
+                .thenReturn(ErrorCode.EMAIL_NOTFOUND.getMessage());
+
+        // when
+        MvcResult result = mockMvc.perform(post(url).with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserDto.RequestChangePassword(email, password))))
+                .andDo(print())
+                .andReturn();
+
+        // then
+        String response = result.getResponse().getContentAsString();
+        String message = JsonPath.parse(response).read("$.message");
+
+        assertThat(message).isEqualTo(ErrorCode.EMAIL_NOTFOUND.getMessage());
+    }
+
 }
