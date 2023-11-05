cb72759a164df861c7747f1b4b6822040f49b728
GiJung <weeds2577@gmail.com>
2023-11-06 02:25:48 +0900

    style(SearchController): change mapping uri plcae -> search

--- src/main/java/me/gijung/HAP/controller/SearchController.java
+++ src/main/java/me/gijung/HAP/controller/SearchController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/place")
+@RequestMapping("/api/v1/search")
 @RequiredArgsConstructor
 public class SearchController {
 
